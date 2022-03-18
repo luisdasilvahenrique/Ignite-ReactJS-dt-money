cdaed19f43e52246b2ac4d0da82e3b5de3f38030
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-03-18 12:36:52 -0300

    creating in dashboard: TransactionTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import { TransactionTable } from "./components/TransactionTable";
 
 export function App() {
   return (
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 :root{
     --background: #f0f2f5;
+    --green: #33CC95;
     --red: #E52E4D;
     --blue: #5429cc;
     
