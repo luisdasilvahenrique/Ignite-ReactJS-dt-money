7447e8cb063baaa56274493272460a0021bf9709
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-03-28 13:55:04 -0300

    listando transações e seeds

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,45 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
-export function TransactionTable(){
-    useEffect(() => {
-      api.get("transactions")
-      .then(response => console.log(response.data))
-    }, [])
-    
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Titulo</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-               
-               <tbody>
-                   <tr>
-                       <td className="title">Desenvolvimento de website</td>
-                       <td className="deposit">R$ 12.000</td>
-                       <td>Desenvolvimento</td>
-                       <td>18/03/2022</td>
-                   </tr>
+export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-                   <tr>
-                       <td>Aluguel</td>
-                       <td className="withrow">R$ -1.100</td>
-                       <td>Casa</td>
-                       <td>17/03/2022</td>
-                   </tr>
-               </tbody>
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
 
-            </table>
-        </Container>
-    );
-}    
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}> {transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createServer, Model } from 'miragejs';
 import { App } from './App';
-import Schema from 'miragejs/orm/schema';
-import { request } from 'https';
 
 createServer({
 
   models: {
     transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withrow',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+      ],
+    })
   },
 
   routes() {
